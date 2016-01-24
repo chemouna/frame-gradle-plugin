b291daf35ab6a468c94d99526cbd56d0975b170e
Mouna Cheikhna <cheikhnamouna@gmail.com>
2016-01-24 15:11:31 +0100

    Setup sample correctly.

--- frame-plugin/src/main/groovy/com/mounacheikhna/screenshots/frame/FrameTask.groovy
+++ frame-plugin/src/main/groovy/com/mounacheikhna/screenshots/frame/FrameTask.groovy
@@ -16,8 +16,10 @@ class FrameTask extends DefaultTask implements FrameSpec {
 
   @TaskAction
   void performTask() {
-    String screenshotsFolder = "${project.projectDir}/$screenshotsDir" //this is the one that can be provided by users
-    String frameFileName = "$framesDir/$selectedFrame"
+    //TODO: provide a clear error to the user when there a trailing / that making folder not recognized
+    String screenshotsFolder = "${project.projectDir}/$screenshotsDir/"
+    //this is the one that can be provided by users
+    String frameFileName = "${project.projectDir}/$framesDir/$selectedFrame"
 
     String deviceFrameRequiredSize = "1270x1290"
     String labelTextSize = "40"
@@ -25,51 +27,45 @@ class FrameTask extends DefaultTask implements FrameSpec {
 
     Map<String, String> screenshotsTitles = titles
 
-    new File(screenshotsFolder).listFiles({ it.isDirectory() } as FileFilter)
-            .each {
-      dir ->
-        dir.eachFileRecurse {
-          if (it.isFile() && it.name.contains(".png")) {
+    new File(screenshotsFolder).eachFileRecurse {
+      if (it.isFile() && it.name.contains(".png")) {
+        String imageFileName = it.name
+        String taskSuffixName = "$imageFileName" //"${dir.name}$imageFileName"
+        String imageDir = it.parent
 
-            String imageFileName = it.name
-            String taskSuffixName = "${dir.name}$imageFileName"
-            String imageDir = it.parent
+        //resize screenshot to frame size
+        project.tasks.create("c1$taskSuffixName", Exec) {
+          workingDir imageDir
+          commandLine "convert", "$imageFileName", "-resize", deviceFrameRequiredSize,
+                  "$imageFileName"
+        }.execute()
 
-            //resize screenshot to frame size
-            project.tasks.create("c1$taskSuffixName", Exec) {
-              workingDir imageDir
-              commandLine "convert", "$imageFileName", "-resize", deviceFrameRequiredSize,
-                      "$imageFileName"
-            }.execute()
+        //put screenshot in a frame
+        project.tasks.create("c2$taskSuffixName", Exec) {
+          workingDir imageDir
+          commandLine "convert", "$frameFileName", "$imageFileName",
+                  "-gravity", "center", "-compose", "over", "-composite",
+                  "-fill", "gold", "-channel", "RGBA", "-opaque", "none", "$imageFileName"
+        }.execute()
 
-            //put screenshot in a frame
-            project.tasks.create("c2$taskSuffixName", Exec) {
-              workingDir imageDir
-              commandLine "convert", "$frameFileName", "$imageFileName",
-                      "-gravity", "center", "-compose", "over", "-composite",
-                      "-fill", "gold", "-channel", "RGBA", "-opaque", "none", "$imageFileName"
-            }.execute()
-
-            def screenshotName = it.name
-            String screenshotsTitle = screenshotsTitles.findResult {
-              key, value -> if (screenshotName.contains(key)) return value
-            }
-            screenshotsTitle = screenshotsTitle ?: ""
-
-            //check if any key of titles is contained in it.name
-            //-> check if any of a map is contained in a string
-            //put background and title
-            project.tasks.create("c3$taskSuffixName", Exec) {
-              workingDir imageDir
-              commandLine "convert", "$imageFileName", "-background", "Gold", "-gravity",
-                      "North",
-                      "-pointsize", "$labelTextSize", "-density", "100", "-fill", "white",
-                      "-annotate", "+0+$topOffset", "${screenshotsTitle}",
-                      "$imageFileName"
-            }.execute()
-
-          }
+        def screenshotName = it.name
+        String screenshotsTitle = screenshotsTitles.findResult {
+          key, value -> if (screenshotName.contains(key)) return value
         }
+        screenshotsTitle = screenshotsTitle ?: ""
+
+        //check if any key of titles is contained in it.name
+        //-> check if any of a map is contained in a string
+        //put background and title
+        project.tasks.create("c3$taskSuffixName", Exec) {
+          workingDir imageDir
+          commandLine "convert", "$imageFileName", "-background", "Gold", "-gravity",
+                  "North",
+                  "-pointsize", "$labelTextSize", "-density", "100", "-fill", "white",
+                  "-annotate", "+0+$topOffset", "${screenshotsTitle}",
+                  "$imageFileName"
+        }.execute()
+      }
     }
   }
 
